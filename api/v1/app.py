90d46c89fd7c8c9f6c64dec058874b1ea5c65909
rajy <kiddo.rajy@gmail.com>
2023-10-31 01:33:37 -0700

    Merge tasks

---
+++ api/v1/app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Flask Application"""
+from flask import Flask, render_template, make_response, jsonify
+from models import storage
+from api.v1.views import app_views
+from os import environ
+from flask_cors import CORS
+from flasgger import Swagger
+from flasgger.utils import swag_from
+
+app = Flask(__name__)
+
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def teardown(exception):
+    storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Error 404"""
+    return make_response(jsonify({'Error': "NOt Found"}), 404)
+
+app.config['SWAGGER'] = {
+    'title': 'AirBnB clone Restful API',
+    'uiversion': 3
+}
+
+Swagger(app)
+
+
+
+if __name__ == "__main__":
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', 5000))
+    app.run(host=host, port=port, threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Create a status route"""
+
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import PLace
+from models.review import Review
+from models.state import State
+from models.user import User
+
+
+@app_views.route('/status', methods=['GET'])
+def get_status():
+    """Returns the status"""
+
+    return jsonify({"status": "OK"})
+
+@app_views.route('/api/v1/stats', methods=['GET'])
+def get_stats():
+    """
+    Retrives the number of each objects by type
+    """
+    classes = [Amenity, City, Place, Review, State, User]
+    names = ["amenities", "cities", "places", "review", "state", "user"]
+
+    num_objs = {}
+    for i in range(len(classes)):
+        num_objs[names[i]] = storage.count(classes[i])
+
+    return jsonify(num_objs)
+
